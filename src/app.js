9402a2fdbddfd82d0c5bd9c94ba90edbdd3530d4
Justin Linn <justinslinn727@gmail.com>
2018-10-11 10:16:10 -0400

    Cleared timers

--- src/app.js
+++ src/app.js
@@ -7,14 +7,20 @@ const beavers = document.querySelectorAll('.beaver');
 /** Selects score so it can be manipulated by startGame() and whack() to reflect current score */
 const scoreBoard = document.querySelector('.score');
 
+/** */
+const startButton = document.querySelector('.start');
+
 /** Global variable that gets reassigned to current index of selected hole to find duplicates */
 let duplicateHole;
+
+let endGameTimeout;
+let peekTimeout;
 
 /** Global variable that gets reassigned to reflect current score */
 let score = 0;
 
 /** Global boolean value that gives current state of game */
-let timeOut = false;
+let isTimeout = false;
 
 /**
  * randomTime() - Generates random time
@@ -46,20 +52,97 @@ function randomHole(holes) {
 function peek() {
   const time = randomTime(1000, 1400);
   const hole = randomHole(holes);
+
   hole.classList.add('peek');
-  setTimeout(() => {
+
+  clearTimeout(peekTimeout);
+  peekTimeout = setTimeout(() => {
     hole.classList.remove('peek');
-    if (!timeOut) peek();
+    if (!isTimeout) peek();
   }, time);
 }
 
-/** startGame() - Set score to zero and starts game for duration of `setTimeout */
+function createInGameButtons() {
+  const container = document.querySelector('.button-container');
+  const resetButton = document.createElement('button');
+  const stopButton = document.createElement('button');
+  stopButton.id = 'stop';
+
+  function resetButtons() {
+    resetButton.style.display = 'none';
+    stopButton.style.display = 'none';
+    startButton.style.display = 'flex';
+    isTimeout = true;
+  }
+
+  function onReset() {
+    resetButtons();
+    startButton.style.display = 'none';
+    // Start counting, 1..2..3..
+    const counter = document.createElement('div');
+    counter.classList.add('counter');
+    counter.textContent = 3;
+    container.prepend(counter);
+    setTimeout(() => {
+      counter.textContent = 2;
+    }, 2000);
+    setTimeout(() => {
+      counter.textContent = 1;
+    }, 3000);
+    setTimeout(() => {
+      counter.textContent = 'GO!!';
+    }, 4000);
+    setTimeout(() => {
+      startGame();
+      resetButton.style.display = 'inline';
+      stopButton.style.display = 'inline';
+      counter.style.display = 'none';
+    }, 4500);
+  }
+
+  resetButton.innerHTML = 'Reset?';
+  resetButton.onclick = onReset;
+  resetButton.classList.add('.reset');
+  if (!isTimeout) {
+    container.prepend(resetButton);
+  }
+
+  stopButton.innerHTML = 'Stop';
+  stopButton.onclick = resetButtons;
+  stopButton.classList.add('.reset');
+  if (!isTimeout) {
+    container.prepend(stopButton);
+  }
+}
+
+startButton.addEventListener('click', createInGameButtons);
+
+/** startGame() - Set score to zero, removes start button
+ * and starts game for duration of setTimeout */
 function startGame() { /*eslint-disable-line*/
+  holes.forEach(hole => hole.classList.remove('.peek'));
+
+  // End the game. after 30s
+  clearTimeout(endGameTimeout);
+  clearTimeout(peekTimeout);
+  endGameTimeout = setTimeout(() => {
+    // Ensure any existing moles on UI are removed
+    isTimeout = true;
+
+    // Remove stop button from DOM
+    document.getElementById('stop').style.display = 'none';
+
+    // Run the game 60 seconds
+  }, 30000);
   scoreBoard.textContent = 0;
-  timeOut = false;
+
+  //   clears pre-existing beavers
+  isTimeout = true;
+  isTimeout = false;
   score = 0;
+
   peek();
-  setTimeout(() => (timeOut = true), 15000);
+  if (startButton) startButton.style.display = 'none';
 }
 
 /** whack() - Increments score by 1 when onClick and checks to make sure input is valid (not a bot) */
